19d65958c56a276f2025f6ca30ea0a4a3fdd48a3
arthur <arthur.teisseire@epitech.eu>
2019-05-07 19:22:15 +0200

    Add print_arrays

--- main.py
+++ main.py
@@ -17,17 +17,46 @@ X2_DIST_TOT = [[0.00, 0.02, 0.06, 0.15, 0.27, 0.45, 0.71, 1.07, 1.64, 2.71, 3.84
 def main(args):
     x_array = [i for i in range(0, 9)]
     prob = sum(map(lambda x: x[0] * x[1], zip(args, x_array))) / N / 100.0
-    print_row(" x", x_array, "Total", "%d")
-    print_row("Ox", args, "100", "%d")
     tx = list(map(lambda x: lb(N, x, prob) * 100, x_array))
-    print_row("Tx", tx, "100", "%.1f")
+
+    print_arrays(args, tx)
     print("Distribution:\tB(100, %.4f)" % prob)
 
 
-def print_row(prefix, array, suffix, format):
+def print_arrays(args, tx):
+    range_array = [[i] for i in range(0, 9)]
+    range_array, args, tx = modify_arrays(range_array, args, tx)
+    range_array = range_array_to_string_array(range_array)
+    print_row(" x", range_array, "Total", "%s")
+    print_row("Ox", args, "100", "%d")
+    print_row("Tx", tx, "100", "%.1f")
+
+
+def modify_arrays(range_array, args, tx):
+    for i in range(len(range_array)):
+        if args[i] < 10 and args[i + 1] < 10:
+            range_array = range_array[:i] + [[range_array[i][0], range_array[i + 1][-1]]] + range_array[i + 2:]
+            args = args[:i] + [args[i] + args[i + 1]] + args[i + 2:]
+            tx = tx[:i] + [tx[i] + tx[i + 1]] + tx[i + 2:]
+            range_array, args, tx = modify_arrays(range_array, args, tx)
+            break
+    return range_array, args, tx
+
+
+def range_array_to_string_array(range_array):
+    string_array = []
+    for i in range_array:
+        if len(i) > 1:
+            string_array.append(str(i[0]) + "-" + str(i[-1]))
+        else:
+            string_array.append(str(i[0]))
+    return string_array
+
+
+def print_row(prefix, array, suffix, format_string):
     print("  %s" % prefix, end="\t| ")
     for x in array:
-        print(format % x, end="\t| ")
+        print(format_string % x, end="\t| ")
     print("%s" % suffix)
 
 
