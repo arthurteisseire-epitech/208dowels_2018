84c4fd10ce4c8e0d3fc62ef6d28f0a254b9671ba
arthur <arthur.teisseire@epitech.eu>
2019-05-07 20:13:00 +0200

    Fix merge

--- print.py
+++ print.py
@@ -1,19 +1,31 @@
 def print_arrays(args, tx):
     range_array = [[i] for i in range(0, 9)]
-    range_array, args, tx = modify_arrays(range_array, args, tx)
+    range_array, args, tx = merge_both_inferior(range_array, args, tx)
+    range_array, args, tx = merge_one_inferior(range_array, args, tx)
     range_array = range_array_to_string_array(range_array)
     print_row(" x", range_array, "Total", "%s")
     print_row("Ox", args, "100", "%d")
     print_row("Tx", tx, "100", "%.1f")
 
 
-def modify_arrays(range_array, args, tx):
+def merge_both_inferior(range_array, args, tx):
+    for i in range(len(range_array) - 1):
+        if args[i] < 10 and args[i + 1] < 10:
+            range_array = range_array[:i] + [[range_array[i][0], range_array[i + 1][-1]]] + range_array[i + 2:]
+            args = args[:i] + [args[i] + args[i + 1]] + args[i + 2:]
+            tx = tx[:i] + [tx[i] + tx[i + 1]] + tx[i + 2:]
+            range_array, args, tx = merge_both_inferior(range_array, args, tx)
+            break
+    return range_array, args, tx
+
+
+def merge_one_inferior(range_array, args, tx):
     for i in range(len(range_array) - 1):
         if args[i] < 10 or args[i + 1] < 10:
             range_array = range_array[:i] + [[range_array[i][0], range_array[i + 1][-1]]] + range_array[i + 2:]
             args = args[:i] + [args[i] + args[i + 1]] + args[i + 2:]
             tx = tx[:i] + [tx[i] + tx[i + 1]] + tx[i + 2:]
-            range_array, args, tx = modify_arrays(range_array, args, tx)
+            range_array, args, tx = merge_one_inferior(range_array, args, tx)
             break
     return range_array, args, tx
 
